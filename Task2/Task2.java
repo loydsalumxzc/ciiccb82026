31dad76622618464e2bc1c183f6d0a833eefb476
loydsalumxzc <charloydsalumbiz@gmail.com>
2026-07-24 16:38:12 +0000

    Finished Task2

--- Task2/Task2.java
+++ Task2/Task2.java
@@ -1,1 +1,13 @@
+public class Task2{
+    public static void main(String[] args) {
+        byte zero = 0;
+        short s = 3;
+        int one = 1;
+        float f = 2.0f;
+        boolean b = true;
+        char c = 'H';
 
+        String result = c + "" + s + one + one + zero + " " + "w" + zero + "r" + one + "d" + " " + f + " " + b;
+        System.out.println(result);
+    }
+}
