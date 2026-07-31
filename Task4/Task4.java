ddfa1d41369a7581c1559e5682be97b5411e3f1b
loydsalumxzc <charloydsalumbiz@gmail.com>
2026-07-31 03:36:23 +0000

    Finished Task 4

--- Task4/Task4.java
+++ Task4/Task4.java
@@ -1,1 +1,22 @@
+package Task4;
 
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
